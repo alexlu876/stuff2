43b754edae69feb6c5ebaa22ad332bdab72b2929
Alex Lu <alexlu876@gmail.com>
2017-09-25 10:49:23 -0400

    o

--- foo.c
+++ foo.c
@@ -12,12 +12,14 @@ int main(){
   }
   cool[9] = 0;
   printf("value cool[%d] is %d\n", i, cool[i]);
-  int *ip;
   int good[10];
+  int *ip1 = cool+9;
+  int *ip2 = good;
   int k;
   for(k = 9; k >= 0; k--){
-    ip = &cool[k];
-    good[9 - k] = *ip;
+    *ip2 = *ip1;
+    *ip2++;
+    *ip1--;
     printf("value good[%d] is %d\n", 9 - k, good[9 - k]);
   }
   return 0;
